fdad07ceb3085547d31a316e80cde6493d07045b
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-21 23:51:04 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,8 +35,7 @@ def select_name_and_series_subgenres_of_authors
   ON authors.id = series.author_id
   JOIN subgenres
   ON subgenres.id = series.subgenre_id
-  GROUP BY authors.name
-  "
+  ;"
 end
 
 def select_series_title_with_most_human_characters
